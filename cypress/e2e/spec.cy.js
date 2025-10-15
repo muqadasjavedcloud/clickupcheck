25d516f11ae22ba6ddee51aba3fd12b05f1a9eb5
muqadasjavedcloud <muqadasjaved101@gmail.com>
2025-10-15 15:41:12 +0500

    first commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://threatmap-frontend.netlify.app/')
+    cy.visit('https://threatmap-frontend.netlify.app/') // test it visit that specific website or not 
   })
 }) 
  
